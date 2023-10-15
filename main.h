afb1453d1eeb23cf900882ad898a6735a51e1f2b
astrovon <perpetual2005@gmail.com>
2023-10-15 14:55:07 -0700

    The printf func which uses the %d,%i format

--- main.h
+++ main.h
@@ -5,9 +5,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define BUFF_SIZE 1024
 
-int _printf(char *format, ...);
+int _printf(const char *format, ...);
 int print_char(char c);
 int print_str(char *str);
-
+int print_int(va_list types, char buffer)
 #endif /* MAIN_H */
